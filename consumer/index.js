41b3922f9825f7dcfbb2130d94f81f10a260d728
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-05-08 18:05:48 +0530

    Starting from the begining and intializing a doc on steps followed for the project

--- consumer/index.js
+++ consumer/index.js
@@ -1,17 +1,17 @@
 console.log("consumer");
 import Kafka from "node-rdkafka";
 
-const consumer = Kafka.KafkaConsumer({
-    'group.id': 'kafka',
-    'metadata.broker.list': 'localhost:9092'
-}, {});
+// const consumer = Kafka.KafkaConsumer({
+//     'group.id': 'kafka',
+//     'metadata.broker.list': 'localhost:9092'
+// }, {});
 
-consumer.connect();
+// consumer.connect();
 
-consumer.on("ready", ()=>{
-    console.log("Consumer ready")
-    consumer.subscribe(['test_kafka_topic_1']);
-    consumer.consume();
-}).on('data', (data) => {
-    console.log(`Message: ${data.value}`)
-})
+// consumer.on("ready", ()=>{
+//     console.log("Consumer ready")
+//     consumer.subscribe(['test_kafka_topic_1']);
+//     consumer.consume();
+// }).on('data', (data) => {
+//     console.log(`Message: ${data.value}`)
+// })
--- producer/index.js
+++ producer/index.js
@@ -1,22 +1,22 @@
 console.log("producer");
 import Kafka from "node-rdkafka"
 
-const stream = Kafka.Producer.createWriteStream({
-    'metadata.broker.list': 'localhost:9092'
-}, {}, {topic: 'test_kafka_topic_1'});
+// const stream = Kafka.Producer.createWriteStream({
+//     'metadata.broker.list': 'localhost:9092'
+// }, {}, {topic: 'test_kafka_topic_1'});
 
-function queueMessage(){
-    const success = stream.write(Buffer.from('hi'));
-    if(success) {
-        console.log("Message sent")
-    }else{
-        console.log("something is wrong")
-    }
-}
+// function queueMessage(){
+//     const success = stream.write(Buffer.from('hi'));
+//     if(success) {
+//         console.log("Message sent")
+//     }else{
+//         console.log("something is wrong")
+//     }
+// }
 
-setInterval(()=>{
-    queueMessage();
-}, 3000);
+// setInterval(()=>{
+//     queueMessage();
+// }, 3000);
 
 
 
