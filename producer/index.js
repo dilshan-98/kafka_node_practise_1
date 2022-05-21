7b0b32866a3fe8be73d6716587f8354cbd3a08fe
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-05-21 15:11:02 +0530

    producer done part1

--- producer/index.js
+++ producer/index.js
@@ -1,22 +1,28 @@
 console.log("producer");
 import Kafka from "node-rdkafka"
 
-// const stream = Kafka.Producer.createWriteStream({
-//     'metadata.broker.list': 'localhost:9092'
-// }, {}, {topic: 'test_kafka_topic_1'});
+const stream = Kafka.Producer.createWriteStream({
+    //Takes an object to define the broker
+    'metadata.broker.list': 'localhost:9092'
+    /*after this, there is a second parameter to define some options (not defined here) and third parameter to 
+    define the topics*/
+}, {}, { topic: 'test_kafka_topic_1' });
 
-// function queueMessage(){
-//     const success = stream.write(Buffer.from('hi'));
-//     if(success) {
-//         console.log("Message sent")
-//     }else{
-//         console.log("something is wrong")
-//     }
-// }
+function queueMessage() {
+    //allocated a buffer for the string "hi"
+    const success = stream.write(Buffer.from("Hi"));
 
-// setInterval(()=>{
-//     queueMessage();
-// }, 3000);
+    if (success) {
+        console.log("Message sent")
+    } else {
+        console.log("something is wrong")
+    }
+};
+
+//setting interval of 3sec to display the message in the loop
+setInterval(() => {
+    queueMessage();
+}, 3000);
 
 
 
