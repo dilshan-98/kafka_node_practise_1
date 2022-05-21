f684b7c6c042bad88e100c31b85259b76c83b82a
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-05-22 02:30:25 +0530

    instead of hardcoding, edited the code to produce random events

--- producer/index.js
+++ producer/index.js
@@ -18,7 +18,16 @@ function queueMessage() {
     */
 
     //defining the event (refer avsc documentation)
-    const event = { category: 'DOG', sound: 'Bark' };
+    //const event = { category: 'DOG', sound: 'Bark' };
+
+    //instead of hard coding events, let's randomize the events generated
+
+    const category = generateRandomAnimal();
+
+    const sound = generateRandomSound(category);
+
+    //since the key and the value are same, no need to specify seperately
+    const event = { category, sound };
 
     //using the buffer method from avsc instead of default buffer
     const success = stream.write(eventType.toBuffer(event));
@@ -35,5 +44,19 @@ setInterval(() => {
     queueMessage();
 }, 3000);
 
+function generateRandomAnimal () {
+    const category = ["DOG", "CAT"];
+    return category[Math.floor(Math.random() * category.length)];
+};
+
+function generateRandomSound (category) {
+    if (category === "DOG") {
+        const sounds = ["bark", "woof"];
+        return sounds[Math.floor(Math.random() * sounds.length)];
+    } else if (category === "CAT") {
+        const sounds = ["purr", "meow"];
+        return sounds[Math.floor(Math.random() * sounds.length)];
+    }
+};
 
 
